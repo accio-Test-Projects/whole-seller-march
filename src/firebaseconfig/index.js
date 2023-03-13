2c95ad8ca4d0b419464ffcda216d3ea79bcf2bf7
sahil jaryal <sahil199926@gmail.com>
2023-03-13 17:48:54 +0530

    .

---
+++ src/firebaseconfig/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAVQuCwhzuGoHPHoJD1Q4ZFCAaytugaxzM",
+  authDomain: "wholeseller-march.firebaseapp.com",
+  projectId: "wholeseller-march",
+  storageBucket: "wholeseller-march.appspot.com",
+  messagingSenderId: "998070951572",
+  appId: "1:998070951572:web:95010d527283eca0371e81"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
